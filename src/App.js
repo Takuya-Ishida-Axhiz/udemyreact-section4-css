74c86277aca91534c737ffc63c2f84b685f2b7f9
Takuya-Ishida-Axhiz <62678869+Takuya-Ishida-Axhiz@users.noreply.github.com>
2022-02-18 13:16:35 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponents } from "./components/StyledComponents";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <StyledComponents />
+    </div>
+  );
+}
